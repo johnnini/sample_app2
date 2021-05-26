7b921879b91bea4687dc6f3db2d2d1a6a5830fb4
johnnini <zoonniezoo@gmail.com>
2021-05-26 18:08:05 +0900

    ８章終わり

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,9 +5,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
-      post users_path, params: { user: { name:"", 
-        email: "user@invalid", 
-        password:"foo", 
+      post users_path, params: { user: { name:"",
+        email: "user@invalid",
+        password:"foo",
         password_confirmation: "bar"}}
     end
     assert_template 'users/new'
@@ -26,6 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
 
 end
